b2dea4249954b08c05bb4ecb54251cf423c5788c
ThomasLaforge <thomas.laforge.38@gmail.com>
2025-11-10 16:47:35 +0100

    add crud recipes

---
+++ middleware/checkToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const checkToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const tokenType = authHeader.split(" ")[0];
+    if( tokenType !== "Bearer") {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    if(!jwt.verify(token, process.env.SECRET)) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    next();
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+}
---
+++ routers/auth.js
@@ -0,0 +1,50 @@
+import { compare, hash } from "bcrypt";
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { db } from "../index.js";
+
+export const authRouter = Router();
+
+authRouter.post("/local/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "Missing username, email or password" });
+  }
+
+  const hashedPassword = await hash(password, 10);
+  try {
+    await db.run("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [username, email, hashedPassword])
+    const user = await db.get("SELECT * FROM users WHERE email = ?", [email]);
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, { expiresIn: '1h' });
+    delete user.password;
+    res.status(201).json({ ...user, jwt:token });
+  }
+  catch(error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+authRouter.post("/local/", async (req, res) => {
+  const { identifier, password } = req.body;
+  if (!identifier || !password) {
+    return res.status(400).json({ error: "Missing identifier or password" });
+  }
+
+  const user = await db.get("SELECT * FROM users WHERE email = ?", [identifier]);
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  const passwordMatch = await compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  const token = jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, { expiresIn: '1h' });
+  delete user.password;
+
+  res.json({ ...user, jwt: token });
+
+});
