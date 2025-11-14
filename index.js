fd8aaf88a65da5686b951194f7bf2ae9074e5864
ThomasLaforge <thomas.laforge.38@gmail.com>
2025-11-14 16:08:40 +0100

    recettes -> recipes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(express.json());
 
 const apiRouter = Router()
-apiRouter.use("/recettes", recipesRouter);
+apiRouter.use("/recipes", recipesRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/users", usersRouter);
 app.use("/api", apiRouter);
